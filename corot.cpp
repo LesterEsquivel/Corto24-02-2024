d8f4e44648878c119a281b8b8c2294130ac9cb4d
LesterEsquivel <160952426+LesterEsquivel@users.noreply.github.com>
2024-02-24 10:52:22 -0600

    Update corot.cpp
    3

--- corot.cpp
+++ corot.cpp
@@ -25,3 +25,23 @@ int main() {
             cout << i << " ";
         }
     }
+    // ejercicio 3
+    int calificacion;
+    cout << "Ingrese una calificacion numerica (0-100): ";
+    cin >> calificacion;
+
+    if (calificacion >= 90 && calificacion <= 100) {
+        cout << "La calificacion es: A" << endl;
+    } else if (calificacion >= 80 && calificacion <= 89) {
+        cout << "La calificacion es: B" << endl;
+    } else if (calificacion >= 70 && calificacion <= 79) {
+        cout << "La calificacion es: C" << endl;
+    } else if (calificacion >= 60 && calificacion <= 69) {
+        cout << "La calificacion es: D" << endl;
+    } else if (calificacion >= 0 && calificacion <= 59) {
+        cout << "La calificacion es: F" << endl;
+    } else {
+        cout << "Calificacion invalida. Por favor, ingrese un numero entre 0 y 100." << endl;
+    }
+    return 0;
+}
