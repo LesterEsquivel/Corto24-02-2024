384ed1e6c7ed7bfb83a24b5ed7e51a4fc07814b6
lesteresquivel <nosd407@gmail.com>
2024-03-01 16:44:13 -0600

    comentario a ejercicio 1

--- corot.cpp
+++ corot.cpp
@@ -4,14 +4,14 @@
 using namespace std;
 // bucle while
 int main() {
-    int N_natural = 0;
+    /*int N_natural = 0;
     int contador = 1;
     while (contador <= 10) {
         N_natural += 1;
         double cuadrado = pow(N_natural, 2);
         cout << N_natural << " al cuadrado es: " << cuadrado << "\n";
         contador += 1;
-    }
+    }*/
  // ejercicio 2
     
         int numero, i, resto;
